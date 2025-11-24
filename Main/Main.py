7c0814c488e79c6c7dfaaa9e5781c5a77b576ca5
gabrielsilva159 <g235038@dac.unicamp.br>
2025-11-24 08:14:12 -0300

    Changing Main

--- Main/Main.py
+++ Main/Main.py
@@ -1,5 +1,5 @@
 from NeuralNetwork import NeuralNetwork
-from NNClasses import Activation_ReLU
+from NNClasses import *
 from DataSet import DataSet
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,18 +9,22 @@ def main():
     seed = 42
     np.random.seed(seed)
 
-    xpts = np.linspace(0, 1, 20)
+    xpts = np.linspace(0, 1, 200)
     npts = len(xpts)
 
     # Generating data
-    yreal = np.sin(2*np.pi*xpts)
+    yreal = (
+            np.sin(2*np.pi*xpts)
+            + 0.3*np.sin(12*np.pi*xpts)      
+    
+        )
 
     Data = DataSet(xpts.reshape(npts, 1), yreal.reshape(npts, 1))
-    train_set, test_set = Data.split(0.5)
+    train_set, test_set = Data.split(0.3)
 
     # Learning rate and number of epochs
-    lr = 0.1
-    epochs = 5000
+    lr = 0.15
+    epochs = 10000
 
     # Creating the neural network
     nn = NeuralNetwork(1, lr=lr, epochs=epochs)
@@ -28,17 +32,23 @@ def main():
     nn_layers = {
                 "layer_0": 
                     {
-                        "neurons": 50, 
-                        "activation": Activation_ReLU(),
+                        "neurons": 512, 
+                        "activation": Activation_LeakyReLU(),
                         "dropout": [False]
                     },
                 "layer_1": 
                     {
-                        "neurons": 25, 
-                        "activation": Activation_ReLU(),
-                        "dropout": [True, 0.2]
+                        "neurons": 512, 
+                        "activation": Activation_LeakyReLU(),
+                        "dropout": [True, 0.4],
                     },
                 "layer_2": 
+                    {
+                        "neurons": 512, 
+                        "activation": Activation_Tanh(),
+                        "dropout": [True, 0.4],
+                    },
+                "layer_3": 
                     {
                         "neurons": 1, 
                         "activation": None,
@@ -52,7 +62,7 @@ def main():
     nn.train(train_set, test_set)
 
     # Plotting results
-    nn.plot_loss()
+    # nn.plot_loss()
 
     # plt.plot(xpts, yreal, 'o', label='True data')
     # plt.plot(xpts, nn.layers[-1].output, '-', label='NN prediction')
@@ -63,6 +73,7 @@ def main():
     plt.plot(xpts, yreal, '-', label='True data')
     plt.scatter(train_set.x, nn.forward(train_set.x), label='NN prediction')
     plt.scatter(test_set.x, nn.forward(test_set.x), label='NN test prediction')
+
     plt.title('Sine fit with neural net')
     plt.legend()
     plt.show()
